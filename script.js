2e5e08c068efd28ec9066dd776bf41eca28f7fad
Jake Swackhammer <j.100101@gmail.com>
2021-09-20 16:58:18 -0400

    Finished prompt user function

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passConfig = new Map();
 
-// Write password to the #password input
+passConfig.set('passwordLength', 0);
+passConfig.set('lowerCase', false);
+passConfig.set('upperCase', false);
+passConfig.set('numeric', false);
+passConfig.set('special', false);
+
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(passConfig);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  return;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", promptUser.bind(null, passConfig));
+
+
+function promptUser(map) {
+  console.log('prompt user started')
+  var passwordLength = prompt("How long would you like the password to be? (min 8 characters, max 128 characters) Please enter a number:", 24)
+
+  if (passwordLength == null) {
+    return
+  } else if(passwordLength > 7 && passwordLength < 129){
+    map.set('passwordLength', passwordLength)
+  } else {
+    alert("Password length must be entered as a number between 8 and 128 (inclusive)")
+    promptUser(map)
+    return
+  }
+
+  var lowerCase = confirm('Do you want lowercase letters?')
+  map.set('lowerCase', lowerCase)
+  var upperCase = confirm('Do you want uppercase letters?')
+  map.set('upperCase', upperCase)
+  var numeric = confirm('Do you want numeric characters?')
+  map.set('numeric', numeric)
+  var special = confirm('Do you want special letters?')
+  map.set('special', special)
+
+  alert(`You picked ${map.get('passwordLength')} characters, ${map.get('lowerCase')} for lowercase.`)
+  return
+}
